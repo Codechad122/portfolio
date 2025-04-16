688a3be0cf720544946db1e6ab9dd329699474af
Codechad122 <footballers737@gmail.com>
2025-04-15 21:31:09 -0500

    update routing

--- src/main.js
+++ src/main.js
@@ -9,12 +9,14 @@ import Home from './components/Home.vue'
 import ProjectOne from './components/ProjectOne.vue'
 import ProjectTwo from './components/ProjectTwo.vue'
 import ProjectThree from './components/ProjectThree.vue'
+import About from './components/About.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/project-one', component: ProjectOne },
   { path: '/project-two', component: ProjectTwo },
   { path: '/project-three', component: ProjectThree },
+  {path: '/about', component: About}
 ]
 
 const router = createRouter({
