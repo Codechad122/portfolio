71387037e2735e9127df09e60b5be515417b654e
Codechad122 <footballers737@gmail.com>
2025-04-20 10:36:16 -0500

    add basic outline for about me page

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,10 +3,14 @@ import Navbar from './Navbar.vue'
 </script>
 
 <template>
-    <header>
-        <Navbar />
-    </header>
-    <main>
-        I am the about page
-    </main>
+  <header>
+    <Navbar />
+  </header>
+  <main class="flex flex-col mx-auto p-2 justify-center items-center gap-4">
+    <h1 class="text-2xl">About Me</h1>
+    <div class="bg-green-100 h-96 w-full">image will go here</div>
+    <div class="bg-purple-100">
+      <p class="w-full">I am an about me description. More info yet to come.</p>
+    </div>
+  </main>
 </template>
