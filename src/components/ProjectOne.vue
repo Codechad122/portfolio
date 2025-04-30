d32e42def07737d170173ed27aebe3d2aa1e67dc
Codechad122 <footballers737@gmail.com>
2025-04-29 20:43:07 -0500

    add drafts for projects 1 and 2

--- src/components/ProjectOne.vue
+++ src/components/ProjectOne.vue
@@ -6,8 +6,8 @@ import Navbar from './Navbar.vue'
   <header>
     <Navbar />
   </header>
-  <main>
-    <div class="mx-auto flex flex-col">
+  <main class="">
+    <div class="mx-auto flex flex-col p-4 gap-4">
       <div class="w-full bg-green-100 justify-center flex">
         <div class="card bg-green-100 shadow-sm h-96">
           <figure class="px-10 pt-10">
@@ -15,8 +15,15 @@ import Navbar from './Navbar.vue'
           </figure>
         </div>
       </div>
-      <div class="bg-purple-100 flex justify-center">
-        description will go here about the project
+      <div class="grid grid-cols-2 gap-4">
+        <div class="place-items-start">
+          I am the description. A small write up about the project will go here
+        </div>
+        <div class="flex flex-col gap-4 place-items-end">
+          <img src="/brew_haven_about_us_page.jpg" class="rounded-md shadow-md" />
+          <img src="/brew_haven_contact_page.jpg" class="rounded-md shadow-md" />
+          <img src="/brew_haven_products_page.jpg" class="rounded-md shadow-md" />
+        </div>
       </div>
     </div>
   </main>
--- src/components/ProjectTwo.vue
+++ src/components/ProjectTwo.vue
@@ -7,7 +7,7 @@ import Navbar from './Navbar.vue'
     <Navbar />
   </header>
   <main>
-    <div class="mx-auto flex flex-col">
+    <div class="mx-auto flex flex-col p-4 gap-4">
       <div class="w-full bg-green-100 justify-center flex">
         <div class="card bg-green-100 shadow-sm h-96 p-2">
           <figure class="">
@@ -15,8 +15,15 @@ import Navbar from './Navbar.vue'
           </figure>
         </div>
       </div>
-      <div class="bg-purple-100 flex justify-center">
-        description will go here about the project
+      <div class="grid grid-cols-2 gap-4">
+        <div class="place-items-start">
+          I am the description. A small write up about the project will go here
+        </div>
+        <div class="flex flex-col gap-4 place-items-end">
+          <img src="/Player_screen_win_1_mini_monsters.jpg" class="rounded-md shadow-md" />
+          <img src="/Welcome_Screen_mini_monsters.jpg" class="rounded-md shadow-md" />
+          <img src="/You_Win_Screen_mini_monsters.jpg" class="rounded-md shadow-md" />
+        </div>
       </div>
     </div>
   </main>
