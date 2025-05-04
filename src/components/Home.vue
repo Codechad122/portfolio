7bb894b914d4815161453d46090fc899e4ff1006
Codechad122 <footballers737@gmail.com>
2025-05-03 23:31:32 -0500

    move pages around

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,7 +35,7 @@ const projects = [
     <div class="carousel h-96 w-96 rounded-md bg-slate-200">
       <div
         v-for="(project, idx) in projects"
-        :id="project.id.slice(1,)"
+        :id="project.id.slice(1)"
         class="carousel-item relative w-full"
       >
         <img :src="project.image" class="w-96 h-96" />
@@ -43,9 +43,118 @@ const projects = [
         <div
           class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
         >
-          <a :href="idx == 0 ? projects[projects.length - 1].id : projects[idx - 1].id" class="btn btn-circle">❮</a>
-          <a :href="idx == projects.length - 1 ? projects[0].id : projects[idx + 1].id" class="btn btn-circle">❯</a>
+          <a
+            :href="idx == 0 ? projects[projects.length - 1].id : projects[idx - 1].id"
+            class="btn btn-circle"
+            >❮</a
+          >
+          <a
+            :href="idx == projects.length - 1 ? projects[0].id : projects[idx + 1].id"
+            class="btn btn-circle"
+            >❯</a
+          >
         </div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-2 mx-auto max-w-6xl w-full gap-4">
+      <div class="flex flex-col gap-4 p-4 col-start-auto">
+        <h1 class="text-2xl">About Me</h1>
+        <p>
+          Nisl arcu, scelerisque neque ut. Tincidunt amet, tempor duis tortor neque auctor dis
+          ipsum. Pretium cras amet odio amet eleifend id sed cras sed. Aliquet risus posuere aliquet
+          imperdiet sit.
+        </p>
+        <button class="btn btn-md rounded-md bg-blue-300">Resume</button>
+      </div>
+
+      <div class="rounded-full w-full place-items-end">
+        <img src="/Mini_monsters_logo.png" />
+      </div>
+
+      <div class="flex flex-col gap-4 p-4 col-start-auto">
+        <h2 class="text-2xl">Facts</h2>
+        <p>4 years of figma</p>
+        <p>3 years of prototyping</p>
+      </div>
+
+      <div class="rounded-full w-full place-items-end">
+        <img src="/Mini_monsters_logo.png" />
+      </div>
+
+      <div class="flex flex-col gap-4 p-4 col-start-auto">
+        <h2 class="text-2xl">My expertise</h2>
+        <div><p>UI/UX Design</p></div>
+        <div><p>Prototyping</p></div>
+        <div><p>Front End Design</p></div>
+        <div><p>Photoshop</p></div>
+        <div><p>Illistrator</p></div>
+        <div><p>Indesign</p></div>
+      </div>
+
+      <div class="rounded-full w-full place-items-end">
+        <img src="/Mini_monsters_logo.png" />
+      </div>
+
+    </div>
+
+    <div class="flex flex-col gap-4">
+      <div class="p-4 bg-[#215900]">Looking to get a free quote contact me here</div>
+      <button class="rounded-md p-4 bg-[#215900]"><a href="/contact">Contact</a></button>
+      <div class="flex gap-4 justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-instagram"
+        >
+          <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+          <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-linkedin"
+        >
+          <path
+            d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+          ></path>
+          <rect x="2" y="9" width="4" height="12"></rect>
+          <circle cx="4" cy="4" r="2"></circle>
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-mail"
+        >
+          <path
+            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+          ></path>
+          <polyline points="22,6 12,13 2,6"></polyline>
+        </svg>
       </div>
     </div>
   </main>
--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import Home from './components/Home.vue'
 import ProjectOne from './components/ProjectOne.vue'
 import ProjectTwo from './components/ProjectTwo.vue'
 import ProjectThree from './components/ProjectThree.vue'
-import About from './components/About.vue'
 import Contact from './components/Contact.vue'
 
 const routes = [
@@ -17,7 +16,6 @@ const routes = [
   { path: '/project-one', component: ProjectOne },
   { path: '/project-two', component: ProjectTwo },
   { path: '/project-three', component: ProjectThree },
-  { path: '/about', component: About },
   { path: '/contact', component: Contact },
 ]
 
